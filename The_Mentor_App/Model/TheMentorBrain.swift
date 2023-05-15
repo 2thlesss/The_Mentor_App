637522b57b48ef42d1c399ea93fb03ddefe5d860
2thlesss <thomas.jadie.reeves@gmail.com>
2023-05-15 07:37:04 -0400

    rolled back and fixed the search feature.

--- The_Mentor_App/Model/TheMentorBrain.swift
+++ The_Mentor_App/Model/TheMentorBrain.swift
@@ -4,25 +4,83 @@
 //
 //  Created by Thomas Jadie Reeves on 5/12/23.
 //
-
 import Foundation
 
-
-// MARK: - TheMentorBrain
-struct TheMentorBrain: Codable {
-    let name, headline, summary: String
+struct TheMentorBrain: Decodable {
+ 
+    let name: String
+    let headline: String
+    let summary: String
     let experience: [Experience]
     let education: Education
-    let certifications, skills: [String]
+    let certifications: [String]
+    let skills: [String]
+    
+   
+
+    struct Experience: Decodable {
+        let title: String
+        let company: String
+        let dates: String
+        let responsibilities: [String]
+    }
+
+    struct Education: Decodable {
+        let degree: String
+        let university: String
+        let dates: String
+    }
+    
+    static let sampleData: [TheMentorBrain] = [
+        TheMentorBrain(
+            
+            name: "Emily Johnson",
+            headline: "Experienced Cybersecurity Project Manager | Ensuring Digital Safety & Risk Mitigation",
+            summary: "Results-driven Cybersecurity Project Manager with a proven track record...",
+            experience: [
+                TheMentorBrain.Experience(
+                    title: "Project Manager, Cybersecurity Division",
+                    company: "XYZ Corporation",
+                    dates: "2018 - Present",
+                    responsibilities: [
+                        "Lead cross-functional teams in executing complex cybersecurity projects from initiation to completion.",
+                        "Develop and implement comprehensive security strategies and policies..."
+                    ]
+                ),
+                TheMentorBrain.Experience(
+                    title: "Senior Security Analyst",
+                    company: "ABC Corporation",
+                    dates: "2015 - 2018",
+                    responsibilities: [
+                        "Conducted security assessments, vulnerability scans, and penetration tests...",
+                        "Assisted in the development and implementation of security policies..."
+                    ]
+                )
+            ],
+            education: TheMentorBrain.Education(
+                degree: "Bachelor of Science in Computer Science",
+                university: "XYZ University",
+                dates: "2011 - 2015"
+            ),
+            certifications: [
+                "Certified Information Systems Security Professional (CISSP)",
+                "Certified Ethical Hacker (CEH)",
+                "Project Management Professional (PMP)"
+            ],
+            skills: [
+                "Project Management",
+                "Risk Assessment",
+                "Security Strategy",
+                "Regulatory Compliance",
+                "Incident Response",
+                "Security Audits",
+                "Vulnerability Assessment",
+                "Penetration Testing",
+                "Stakeholder Management",
+                "Team leadership"
+            ]
+            
+        )
+    ]
 }
 
-// MARK: - Education
-struct Education: Codable {
-    let degree, university, dates: String
-}
-
-// MARK: - Experience
-struct Experience: Codable {
-    let title, company, dates: String
-    let responsibilities: [String]
-}
